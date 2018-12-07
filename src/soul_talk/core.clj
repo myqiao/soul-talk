d671a15e17c5780730cb6f7915392e698022d077
Zuo Guang <wlnh_2004@163.com>
2018-12-07 10:42:28 +0800

    添加自定义【不缓存头信息】中间件

--- src/soul_talk/core.clj
+++ src/soul_talk/core.clj
@@ -9,5 +9,16 @@
                 (:remote-addr request) 
                 "</body></html>")))
 
+;; 自定义中间件：加入不缓存头信息
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
+
+
 (defn -main []
-  (jetty/run-jetty home-handle  {:port 3000 :join? false}))
+  (jetty/run-jetty 
+    (wrap-nocache home-handle)  
+    {:port 3000 :join? false}))
