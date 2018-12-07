f506703cd6153c722536ff2d666003d201d3d932
Zuo Guang <wlnh_2004@163.com>
2018-12-07 09:49:52 +0800

    使用 Ring 库

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 
+                 [ring "1.7.1"]]
   
   
   
   
-  :main soul-talk.core/foo
+  :main soul-talk.core
   
   
   :profiles {
--- src/soul_talk/core.clj
+++ src/soul_talk/core.clj
@@ -1,6 +1,12 @@
-(ns soul-talk.core)
+(ns soul-talk.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn home-handle [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<html><body>your IP is "
+              (:remote-addr request)
+              "</body></html>")})
+
+(defn -main []
+  (jetty/run-jetty home-handle  {:port 3000 :join? false}))
