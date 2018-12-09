cd0b9afe9fbcafc2acc8ce79b2172c2ec7101916
Zuo Guang <wlnh_2004@163.com>
2018-12-09 14:41:50 +0800

    使用原生 DOM 接口进行客户端验证

--- src-cljs/soul_talk/core.cljs
+++ src-cljs/soul_talk/core.cljs
@@ -1,7 +1,17 @@
 (ns soul-talk.core)
 
-(defn main []
-  (enable-console-print!)
-  (prn "Hello, Clojurescript"))
+;; 表单提交处理函数：账户和密码不能为空
+(defn validate-form []
+  (let [email (.getElementById js/document "email")
+        password (.getElementById js/document "password")]
+    (if (and (> (count (.-value email)) 0) (> (count (.-value password)) 0))
+      true
+      (do (js/alert "email和密码不能为空")
+          false))))
 
-(main)
+;; 为 Form 绑定 onsubmit 处理函数
+;; 导出该函数
+(defn ^:export init []
+  (if (and js/document (.-getElementById js/document))
+    (let [login-form (.getElementById js/document "loginForm")]
+      (set! (.-onsubmit login-form) validate-form))))
